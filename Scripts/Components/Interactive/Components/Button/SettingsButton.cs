d3cccb3029d1dcfd7fffa9136b3ea22b3412c4ed
Petrushevski Apps <petrushevskiapps@gmail.com>
2025-08-23 22:40:14 +0200

    Implemented Settings button

---
+++ Scripts/Components/Interactive/Components/Button/SettingsButton.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TwoOneTwoGames.UIManager.Components.Interactive;
+using TwoOneTwoGames.UIManager.ScreenNavigation;
+using UnityEngine;
+using Zenject;
+
+[RequireComponent(typeof(UIButton))]
+public class SettingsButton : MonoBehaviour
+{
+    private UIButton _uiButton;
+    private IPopupNavigation _popupNavigation;
+
+    [Inject]
+    private void Initialize(IPopupNavigation popupNavigation)
+    {
+        _popupNavigation = popupNavigation;
+    }
+    private void Awake()
+    {
+        _uiButton = GetComponent<UIButton>();
+        _uiButton.SetData(new UIButtonViewData(
+            isVisible: true, 
+            clickAction: SettingsClicked));
+    }
+
+    private void SettingsClicked()
+    {
+        _popupNavigation.ShowSettingsPopup();
+    }
+}
