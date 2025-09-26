ba59f9b2b838ead7825160efe9c011bd11d03385
Petrushevski Apps <petrushevskiapps@gmail.com>
2025-09-26 12:28:49 +0200

    Implemented Video Player Extension and Data

---
+++ Scripts/Data/ExtensionsData/VideoPlayerViewData.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine.Video;
+
+namespace TwoOneTwoGames.UIManager.Data.ExtensionsData
+{
+    public struct VideoPlayerViewData
+    {
+        public VideoClip VideoClip;
+        public bool IsLooping;
+        public bool IsAutoPlay;
+    }
+}
---
+++ Scripts/Utilitis/Extensions/VideoPlayerExtensions.cs
@@ -0,0 +1,18 @@
+﻿using TwoOneTwoGames.UIManager.Data.ExtensionsData;
+using UnityEngine.Video;
+
+namespace TwoOneTwoGames.UIManager.Utilities.Extensions
+{
+    public static class VideoPlayerExtensions
+    {
+        public static void SetData(this VideoPlayer videoPlayer, VideoPlayerViewData data)
+        {
+            videoPlayer.clip = data.VideoClip;
+            videoPlayer.isLooping = data.IsLooping;
+            if (data.IsAutoPlay)
+            {
+                videoPlayer.Play();
+            }
+        }
+    }
+}
