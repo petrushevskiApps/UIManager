dbc33e6533d15f0afe1a0ecbddcb59c71d3f886f
Petrushevski Apps <petrushevskiapps@gmail.com>
2025-09-26 12:41:14 +0200

    Added Tutorial Popup Data scriptable object

---
+++ Scripts/Data/Tutorial/TutorialPopupData.cs
@@ -0,0 +1,24 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.Video;
+
+namespace TwoOneTwoGames.UIManager.Data.Tutorial
+{
+    [CreateAssetMenu(
+        fileName = "TutorialPopupData-Level_",
+        menuName = "Game Data/Tutorial Popup/Tutorial Popup Data",
+        order = 1)]
+    public class TutorialPopupData: ScriptableObject
+    {
+        [field: SerializeField]
+        public string Title { get; [UsedImplicitly] private set; }
+        [field: SerializeField]
+        public string Message { get; [UsedImplicitly] private set; }
+        [field: SerializeField]
+        public string ButtonText { get; [UsedImplicitly] private set; }
+        [field: SerializeField]
+        public Color ButtonTextColor { get; [UsedImplicitly] private set; }
+        [field: SerializeField]
+        public VideoClip TutorialClip { get; [UsedImplicitly] private set; }
+    }
+}
