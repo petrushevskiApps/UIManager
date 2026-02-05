74bf288542e9fb92dd618aa434385dbf05322d9f
Petrushevski Apps <petrushevskiapps@gmail.com>
2026-02-05 15:33:50 +0100

    Rearanged interface

--- Scripts/Interfaces/GameEconomy/IUiGameEconomyPresenter.cs
+++ Scripts/Interfaces/GameEconomy/IUiGameEconomyPresenter.cs
@@ -5,7 +5,7 @@ namespace TwoOneTwoGames.UIManager.Interfaces
     public interface IUiGameEconomyPresenter
     {
         event EventHandler<(int, float)> EarnedResourceEvent;
-        int GetResourceValueWithId(int id);
         event EventHandler<(int, float)> UsedResourceEvent;
+        int GetResourceValueWithId(int id);
     }
 }
