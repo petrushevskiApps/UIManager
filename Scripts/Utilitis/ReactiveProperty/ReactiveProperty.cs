cf128edd3bf1c1f7401731f8c577391d7114eca4
Petrushevski Apps <petrushevskiapps@gmail.com>
2026-07-27 00:27:50 +0200

    Improved Reactive Property

--- Scripts/Utilitis/ReactiveProperty/ReactiveProperty.cs
+++ Scripts/Utilitis/ReactiveProperty/ReactiveProperty.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TwoOneTwoGames.UIManager.Utilities.ReactiveProperty
 {
     public class ReactiveProperty<T> : IReactiveProperty<T>
     {
+        // Compared through the generic comparer, not object.Equals: the static overload takes two
+        // objects, so every set of a struct T boxed both operands and then fell back to the
+        // reflection-based ValueType.Equals. View data structs are set per frame.
+        private static readonly EqualityComparer<T> Comparer = EqualityComparer<T>.Default;
+
         private readonly bool _alwaysUpdate;
         private T _value;
 
@@ -18,7 +24,7 @@ namespace TwoOneTwoGames.UIManager.Utilities.ReactiveProperty
             get => _value;
             set
             {
-                if (!_alwaysUpdate && Equals(_value, value)) return;
+                if (!_alwaysUpdate && Comparer.Equals(_value, value)) return;
                 _value = value;
                 ValueChanged?.Invoke(_value);
             }
